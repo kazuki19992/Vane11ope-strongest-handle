ace5777eebcf0d019c6bc085d2248d2868057556
Yuta Nakamizo <yuta.nakamizo@ggtk.app>
2020-07-18 08:27:28 +0900

    #5 ES6 っぽくしたのと手続き改善

--- JS/tweet-share.js
+++ JS/tweet-share.js
@@ -1,53 +1,30 @@
 // SNSボタンを追加するエリア
-let area = document.getElementById('vane11ope_share');
+const area = document.getElementById('vane11ope_share');
 // シェア時に使用する値
-let url = location.href; // 現在のページURLを使用する場合 location.href;
-let shareTextContent = document.getElementById('result');
-let text = shareTextContent.innerText;
-let tags = '#ぼくの考えた最強のヴァネロピ'
-// シェアボタンを生成する関数
-function generate_share_button() {
-
+const default_url = location.href; // 現在のページURLを使用する場合 location.href;
+const shareTextContent = document.getElementById('result');
+const default_text = shareTextContent.innerText;
+const default_tags = '#ぼくの考えた最強のヴァネロピ'
+// シェアボタンを更新する関数
+const update_share_button = ({ text: _text, url: _url, tags: _tags } = {}) => {
+    const text = _text || default_text;
+    const url = _url || default_url;
+    const tags = _tags || default_tags;
+    
     // 各シェアボタンのリンク先
-    let twHref = 'https://twitter.com/share?text='+encodeURIComponent(text)+'%0a'+encodeURIComponent(tags)+'%0a'+'&url='+encodeURIComponent(url);
-    let liHref = 'https://line.me/R/msg/text/?'+encodeURIComponent(text)+' '+encodeURIComponent(url);
+    const twHref = `https://twitter.com/share?text=${encodeURIComponent(text)}%0a${encodeURIComponent(tags)}%0a&url=${encodeURIComponent(url)}`;
+    const liHref = `https://line.me/R/msg/text/?${encodeURIComponent(text)} ${encodeURIComponent(url)}`;
 
-    // シェアボタンにリンクを追加
-    let twBtnContent = `<div class="share-btn-Twitter waves-effect waves-light">
-                            <div>
-                                <span class="btn_icon-share"><i class="fab fa-twitter fa-fw fa-2x"></i></span><br>
-                                Twitterでヴァネロピを自慢する！
-                            </div>
-                        </div>`;
+    // シェアボタンのリンクを変更
+    document.getElementById('vane11ope_share_twitter').href = twHref;
+    document.getElementById('vane11ope_share_line').href = liHref;
+};
 
-    let liBtnContent = `<div class="share-btn-line waves-effect waves-light">
-                            <div>
-                                <span class="btn_icon-share"><i class="fab fa-line fa-fw fa-2x"></i></span><br>
-                                LINEでヴァネロピを自慢する！
-                            </div>
-                        </div>`;
-
-    let clickEv = 'onclick="popupWindow(this.href); return false;"';
-    let twLink = '<a href="' + twHref + '" ' + clickEv + 'onclick="reload_content()">' + twBtnContent + '</a>';
-    let liLink = '<a href="' + liHref + '" target="_blank" onclick="reload_content()">' + liBtnContent + '</a>';
-
-    // シェアボタンを挿入
-    area.insertAdjacentHTML(
-        'afterbegin',
-        '<div class="left twitter-btn">' + twLink + '</div>'
-        + '<div class="right line-btn">' + liLink + '</div>'
-    );
-}
-
-function reload_content(){
-    let element = document.getElementById("vane11ope_share");
-    while (element.firstChild) {
-        element.removeChild(element.firstChild);
-    }
-    generate_share_button();
-}
+const reload_content = text => {
+    update_share_button({ text });
+};
 
 // クリック時にポップアップで表示させる関数
-function popupWindow(url) {
+const popupWindow = url => {
     window.open(url, '', 'width=580,height=400,menubar=no,toolbar=no,scrollbars=yes');
 }
